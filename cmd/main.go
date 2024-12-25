55643a698011a06817543bf974d8735b54cbfa9a
Vlad <Vladroon27@gmail.com>
2024-12-25 12:43:23 +0300

    change main.go

--- cmd/main.go
+++ cmd/main.go
@@ -62,10 +62,9 @@ func main() {
 	signal.Notify(exitSig, syscall.SIGINT, syscall.SIGTERM)
 	<-exitSig
 
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
 	go func() {
-		wg := &sync.WaitGroup{}
-		wg.Add(1)
-
 		defer wg.Done()
 
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
